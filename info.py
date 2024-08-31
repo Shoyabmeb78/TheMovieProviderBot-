be553177a9fe34eb2a8b0b4bfed8a2e6f5fd2eb7
Shoyabmeb78 <shoyabmev14@gmail.com>
2024-08-31 19:38:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -116,8 +116,8 @@ MULTI_CLIENT = False
 name = str(environ.get('name', 'LazyPrincess'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
-    ON_HEROKU = True
-    APP_NAME = str(getenv('APP_NAME'))
+    ON_HEROKU = False
+    ATruAME = str(getenv('APP_NAME'))
 
 else:
     ON_HEROKU = False
