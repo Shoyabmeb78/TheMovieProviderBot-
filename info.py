64624e98774107eaefd1b8af413247a838a151d2
Shoyabmeb78 <shoyabmev14@gmail.com>
2024-08-31 17:14:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -105,8 +105,8 @@ if 'DYNO' in environ:
     ON_HEROKU = False
     ATAME = environ.get('APP_NAME')
 else:
-    ON_HEROKU = False
-FQDN = str(getenv('FQDN', '')) or BIND_ADRESS or APP_NAME+'.heroku.com'
+    ON_HEROKU = True
+FQDNFar(getenv('FQDN', '')) or BIND_ADRESS or APP_NAME+'.heroku.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
@@ -116,11 +116,11 @@ MULTI_CLIENT = False
 name = str(environ.get('name', 'LazyPrincess'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
-    ON_HEROKU = False
+    ON_HEROKU = True
     APP_NAME = str(getenv('APP_NAME'))
 
 else:
-    ON_HEROKU = False
+    ON_HEROKU = True
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
     URL = "https://{}/".format(FQDN)
