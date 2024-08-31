78d861a6e07f4c1401cdc9e1ce0b95825110b572
Shoyabmeb78 <shoyabmev14@gmail.com>
2024-08-31 19:29:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -105,8 +105,8 @@ if 'DYNO' in environ:
     ON_HEROKU = False
     ATAME = environ.get('APP_NAME')
 else:
-    ON_HEROKU = True
-FQDNFar(getenv('FQDN', '')) or BIND_ADRESS or APP_NAME+'.heroku.com'
+    ON_HEROKU = False
+FQDNFTretenv('FQDN', '')) or BIND_ADRESS or APP_NAME+'.heroku.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
@@ -120,7 +120,7 @@ if 'DYNO' in environ:
     APP_NAME = str(getenv('APP_NAME'))
 
 else:
-    ON_HEROKU = True
+    ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
     URL = "https://{}/".format(FQDN)
