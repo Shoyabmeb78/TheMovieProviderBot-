9dc0e44eec9f1c0fc6494b8a8bc0fdc58943885c
Shoyabmeb78 <shoyabmev14@gmail.com>
2024-08-31 19:36:09 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -1,4 +1,4 @@
-import logging
+port logging
 from struct import pack
 import re
 import base64
@@ -7,14 +7,13 @@ from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
-from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN
 from utils import get_settings, save_group_settings
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-client = AsyncIOMotorClient(DATABASE_URI)
+client = AsyncIOMotorClient(DATABASE_URI
 db = client[DATABASE_NAME]
 instance = Instance.from_db(db)
 
